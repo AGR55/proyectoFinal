122c297bc1502af1da44600073809bb980b8ea90
AGR55 <gonzalezreyesadriano@gmail.com>
2023-10-28 18:10:30 +0200

    inciso c

--- src/APP/RedHidraulica.java
+++ src/APP/RedHidraulica.java
@@ -13,7 +13,6 @@ public class RedHidraulica {
 
     public int getCantidadDepositos() {
         int aux = 0;
-
         for (int i = 0; i < depositos.size(); i++) {
             if (depositos.get(i) != null) {
                 aux++;
@@ -24,7 +23,6 @@ public class RedHidraulica {
 
     public int getCantidadMotores() {
         int aux = 0;
-
         for (int i = 0; i < motores.size(); i++) {
             if (motores.get(i) != null) {
                 aux++;
@@ -72,5 +70,59 @@ public class RedHidraulica {
                 serial.guardarTanquesRegularOMalEstado(depositos);
             }
         }
+    }
+
+    public List<Integer> capacidadOrdenada() {
+        String[] orden = { "Fibrocemento", "Metal", "Plastico" };
+        List<Cisternas> auxCis = getCisternas();
+        List<Tanques> auxTan = getTanques();
+        List<Integer> capacidadOrdenada = new ArrayList<>();
+        int cantidadCisternas = getCisternas().size();
+        int cantidadTanques = getTanques().size();
+
+        for (int i = 0; i < orden.length; i++) {
+            for (int j = 0; j < cantidadTanques; j++) {
+                if (auxTan.get(j).getMaterial().equals(orden[i])) {
+                    capacidadOrdenada.add(auxTan.get(j).getCapacidad());
+                }
+            }
+        }
+        for (int i = 0; i < cantidadCisternas; i++) {
+            if (auxCis.get(i) instanceof Simples) {
+                capacidadOrdenada.add(auxCis.get(i).getCapacidad());
+            }
+        }
+        for (int i = 0; i < cantidadCisternas; i++) {
+            if (auxCis.get(i) instanceof Compuestas) {
+                capacidadOrdenada.add(auxCis.get(i).getCapacidad());
+            }
+        }
+        return capacidadOrdenada;
+    }
+
+    public String[] cantidadMedios() {
+        String[] medios = { "Bombas", "Turbinas", "Cisternas", "Tanques" };
+        int cantidadBombas = 0;
+        int cantidadTurbinas = 0;
+        int cantidadCisternas = getCisternas().size();
+        int cantidadTanques = getTanques().size();
+
+        for (int i = 0; i < motores.size(); i++) {
+            if (motores.get(i) != null) {
+                if (motores.get(i) instanceof BombasDeAgua) {
+                    cantidadBombas++;
+                } else {
+                    cantidadTanques++;
+                }
+            }
+        }
+
+        int[] cantidades = { cantidadBombas, cantidadTurbinas, cantidadCisternas, cantidadTanques };
+        String[] info = new String[medios.length];
+
+        for (int i = 0; i < cantidades.length; i++) {
+            info[i] = "La cantidad de " + medios[i] + " es" + cantidades[i];
+        }
+        return info;
     }
 }
